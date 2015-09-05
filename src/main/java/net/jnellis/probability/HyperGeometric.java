0f4ca2c08caa1d640e7866b3b4e326c11fbc9306
Joe Nellis <joe@jnellis.net>
2015-09-05 12:52:04 -0700

    Change back from stream based approach. To achieve a non-terminal operation on a stream of ranges came at a performance cost.

--- src/main/java/net/jnellis/probability/HyperGeometric.java
+++ src/main/java/net/jnellis/probability/HyperGeometric.java
@@ -1,17 +1,63 @@
+/*
+ * HyperGeometric.java
+ *
+ * Copyright (c) 2015. Joe Nellis
+ * Distributed under MIT License. See accompanying file License.txt or at
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.jnellis.probability;
 
-import java.util.PrimitiveIterator;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 /**
- * The Hypergeometric probability distribution.
+ * The <a href="https://en.wikipedia.org/wiki/Hypergeometric_distribution">
+ * Hypergeometric probability distribution.</a>
+ * <p>
+ * This probability is the one used to compute odds in
+ * many of the lotteries in the United States.  It is used when
+ * you are picking from two sets in a mixed population. The
+ * basic example is a bag filled with red and black chips. 5 red
+ * chips and 4 black ones.  You are asked to choose 3 chips
+ * without looking.  What is the chance you will pick exactly
+ * two black chips.  If you look below you will see the construction
+ * parameters.  The population size, N, is 4 + 5 = 9. The sample size,
+ * n, is 3 since we are picking 3 chips.  The sample set we are interested
+ * in is black chips which there are 3 of so r = 3.  And finally the
+ * random variable, y, is 2, the number of black chips we want to choose out of
+ * 3 pulls.  Chips are not replaced between pulls.
+ * </p>
+ * <code>
+ * double probability = HyperGeometric.probability(9, 3, 3, 2)
+ * </code>
+ * <p>
+ * To compute lottery odds lets assume the lottery in question has
+ * 50 numbers and they choose 6 of them at random live on television.
+ * So we know N = 50 and the sample size n=6. You have 6 numbers you paid
+ * money for that you are interested in, these six numbers represent the
+ * selected set r, r = 6.  Now for the grand payoff, getting all six numbers
+ * you need all picked numbers to be in your selected set.  y = 6.  Often there
+ * is a lesser payout for matching less than six numbers.
+ * Finally you usually win your money back if you match two numbers so here
+ * y = 2.  To find the chance of winning anything you would compute
+ * P(y &gt;= 2) since usually matching one number has no payout and zero matches
+ * is definitely not paying out.
+ * </p>
+ * <pre>
+ * // Chance to win anything is when P(y &gt; 2)
+ * HyperGeometric cumulative = new HyperGeometric(greaterThanOrEqual,50,6,6)
+ * double result = cumulative.getResult(2);
+ * // it's a 14.572% chance to win anything back
+ *
+ * // Here's a static approach that doesn't create a HyperGeometric object.
+ * CumulativeOperation.greaterThanOrEqual
+ *                    .apply(rv-&gt; HyperGeometric.probability(50,6,6, rv), 2);
+ *
+ * </pre>
  */
 public class HyperGeometric extends DiscreteProbability {
 
-  final int N;
-  final int n;
-  final int r;
+  private final int N;
+  private final int n;
+  private final int r;
 
   /**
    * The Hypergeometric probability distribution.
@@ -31,15 +77,13 @@ public class HyperGeometric extends DiscreteProbability {
     this.r = successStates;
   }
 
-  // The equation: (rCy)*((N-r)C(n-y))/(NCn)
-  // r!/(r-y)!y!  *   (N-r)!/(N-r-(n-y))!(n-y)!  /  N!/(N-n)!n!
-/* Computing the result
-  The computation is composed of pure combinations which are
-	handled in the same fashion as the Binomial and NegativeBinomial
-	probabilities.  The equation is broken up into numerators and
-	denominators and then appropriately multiplied by or divided by
-	depending on the running result being above or below 1.0.
-*/
+  /* Computing the result:
+    The computation is composed of pure combinations which are
+    handled in the same fashion as the Binomial and NegativeBinomial
+    probabilities.  The equation is broken up into numerators and
+    denominators and then appropriately multiplied by or divided by
+    depending on the running result being above or below 1.0.
+  */
   @Override
   public double computeResult(int randomVariable) {
     return probability(N, n, r, randomVariable);
@@ -47,71 +91,98 @@ public class HyperGeometric extends DiscreteProbability {
 
   /**
    * Computes the HyperGeometric probability of a random variable.
+   * <pre>
+   *          (rCy) * ((N-r)C(n-y))
+   * which is --------------------- where xCy is the combination 'x choose y'
+   *                 (NCn)
+   *
+   *              x!
+   * xCy = -------------
+   *         (x-y)! * y!
+   * </pre>
+   *
    * @param N Population size
    * @param n sample size
    * @param r number of success states in population.
    * @param y the number of success states we're interested in.
-   * @return  The probability of this event, between 0.0 and 1.0 inclusive.
+   * @return The probability of this event, between 0.0 and 1.0 inclusive.
    */
   public static double probability(int N, int n, int r, int y) {
-    if (N == 0)
-      return 0.0;
-    assert (y <= r);
-    assert (n - y <= N - r);
-    assert (y >= 0);
-    assert (N > 0);
-    assert (r >= 0);
-    assert (n >= 0);
+    assert (y >= 0) : "Random variable must be greater than or equal to zero.";
+    assert (N > 0) : "Population size must be greater than zero.";
+    assert (r >= 0) : "Success states must be greater than or equal to zero.";
+    assert (n >= 0) : "Sample size must be greater than or equal to zero.";
+    assert (y <= r) : "Expected success must be <= possible success states.";
+    // The number of failure states (n-y) must be less than or equal
+    // to the number of total failure states possible (N-r).
+    if (!(n - y <= N - r)) {
+      return 0.0;  // otherwise 0% chance this event happens.
+    }
+    //break equation up into ranges of numerators and denominators
+    int numer1 = r;
+    int numer2 = N - r;
+    int numer3 = N;
 
-    //cancellation optimization on (rCy)
-    int range1 = Integer.min(r - y, y);
-    //cancellation optimization on ((N-r)C(n-y))
-    int range2 = Integer.min((N - r) - (n - y), n - y);
-    //cancellation optimization on (NCn)
-    int range3 = Integer.min(N - n, n);
-
+    // The equation: (rCy)*((N-r)C(n-y))/(NCn)
     // r!/(r-y)!y!  *   (N-r)!/(N-r-(n-y))!(n-y)!  /  N!/(N-n)!n!
+    //cancellation optimization on (rCy)
+    final int range1 = Integer.min(r - y, y);
+    //cancellation optimization on ((N-r)C(n-y))
+    final int range2 = Integer.min((N - r) - (n - y), n - y);
+    //cancellation optimization on (NCn)
+    final int range3 = Integer.min(N - n, n);
+
+    final int numer1floor = numer1 - range1;
+    final int numer2floor = numer2 - range2;
+    final int numer3floor = numer3 - range3;
+
+    int denom1 = range1;
+    int denom2 = range2;
+    int denom3 = range3;
+
+    // r!/(r-y)!y!  *   (N-r)!/(N-r-(n-y))!(n-y)!  * (N-n)!n!/N!
     // swap numerator and denominator on last component, N!/((N-n)!n!)
-    // Create two streams representing factors of the numerator and denominator
-    // r-range1+1:r   *   (N-r)-range2+1:(N-r)   *   1:range3   // numerators
-    PrimitiveIterator.OfInt numerators =
-        Stream.of(IntStream.rangeClosed(r - range1 + 1, r),
-                  IntStream.rangeClosed(N - r - range2 + 1, N - r),
-                  IntStream.rangeClosed(2, range3)) // start at 2
-              .flatMapToInt(IntStream::sequential)
-              .iterator();
-
-    // 1:range1   *   1:range2    *    N-range3+1 : N        // denominators
-    PrimitiveIterator.OfInt denominators =
-        Stream.of(IntStream.rangeClosed(2, range1), // instead of 1
-                  IntStream.rangeClosed(2, range2),
-                  IntStream.rangeClosed(N - range3 + 1, N))
-              .flatMapToInt(IntStream::sequential)
-              .iterator();
-
-
     double result = 1.0;
-    while (numerators.hasNext() || denominators.hasNext()) {
-      if (result >= 1.0 || !numerators.hasNext()) {
-        result /= denominators.nextInt();
+    while (numer3 > numer3floor || denom3 > 0) {
+      if (result > 1.0 || denom3 == 0) {
+        if (denom1 > 0)
+          result = result / denom1--;
+        else if (denom2 > 0)
+          result = result / denom2--;
+        else
+          result = result / numer3--;
       } else {
-        result *= numerators.nextInt();
+        if (numer1 > numer1floor)
+          result = result * numer1--;
+        else if (numer2 > numer2floor)
+          result = result * numer2--;
+        else
+          result = result * denom3--;
       }
     }
     return result;
   }
 
-
-  // The expected value or population mean is defined by:
-// E(Y) = n*r/N
+  /**
+   * The expected value or population mean is defined by:
+   * E(Y) = sampleSize*#successStates / populationSize
+   *
+   * @return The expected value for this probability distribution.
+   */
   @Override
   double getExpectedValue() {
     return 1.0 * n * r / N;
   }
 
-
-  // The variance is defined by:
-// V(Y) = n * (r/N) * (N-r)/N * (N-n)/(N-1)
+  /**
+   * The variance is defined by:
+   * V(Y) = n * (r/N) * (N-r)/N * (N-n)/(N-1)
+   * Where N is the population size,
+   * n is the sample size,
+   * r is the number of success states
+   *
+   * @return The variance (?2) of the probability distribution.
+   */
   @Override
   double getVariance() {
     if (N == 1)
--- src/test/groovy/net/jnellis/probability/HyperGeometricTest.groovy
+++ src/test/groovy/net/jnellis/probability/HyperGeometricTest.groovy
@@ -1,9 +1,18 @@
+/*
+ * HyperGeometricTest.groovy
+ *
+ * Copyright (c) 2015. Joe Nellis
+ * Distributed under MIT License. See accompanying file License.txt or at
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.jnellis.probability
 
 import spock.lang.Specification
 import spock.lang.Unroll
 
 import static net.jnellis.probability.CumulativeOperation.equal
+import static net.jnellis.probability.CumulativeOperation.lessThan
 import static net.jnellis.probability.CumulativeOperation.lessThanOrEqual
 
 /**
@@ -16,19 +25,24 @@ class HyperGeometricTest extends Specification {
   double resolution = 1.0E-12
 
   @Unroll
-  def "test #N samples, #n samplesChosen, #r specialSamples, P(y=#rv) is #result"() {
+  def "test #N samples, #n samplesChosen, #r specialSamples, P(y=#y) is #result"() {
     setup:
 
     expect:
-    Math.abs(new HyperGeometric(equal, N, n, r).getResult(rv) - result) < resolution
+    double probability = new HyperGeometric(equal, N, n, r).getResult(y)
+    Math.abs(probability - result) < resolution
 
     where:
-    N   | n   | r  | rv | result
-    1   | 1   | 1  | 1  | 1
-    9   | 3   | 3  | 3  | 0.011904761904761897
-    55  | 31  | 5  | 3  | 0.35662697149933564
-    277 | 157 | 57 | 33 | 0.116862766002871
-    301 | 300 | 30 | 30 | 0.900332225913621
+    N   | n   | r  | y  | result
+    1   | 1   | 1  | 0  | 0d
+    1   | 1   | 1  | 1  | 1d
+    2   | 1   | 1  | 0  | 0.5d
+    2   | 1   | 1  | 1  | 0.5d
+    9   | 3   | 3  | 3  | 0.011904761904761897d
+    55  | 31  | 5  | 3  | 0.35662697149933564d
+    277 | 157 | 57 | 33 | 0.116862766002871d
+    301 | 300 | 30 | 30 | 0.900332225913621d
+    10  | 7   | 5  | 1  | 0d
   }
 
   def "test chance to only win two bucks back in lottery"() {
@@ -36,23 +50,74 @@ class HyperGeometricTest extends Specification {
     def N = 50 // fifty numbers
     def n = 6  // of which only six are chosen
     def r = 6  // and of which six are special.
-    def y = 2  // probabilty to only match two numbers
+    def y = 2  // probability to only match two numbers
     def result = new HyperGeometric(equal, N, n, r).getResult(y);
     expect:
     Math.abs(result - 0.128141932073477) < resolution
   }
 
+  def "test chance to win anything back in lottery P(y>=2)"() {
+    setup:
+    int N = 50 // fifty numbers
+    int n = 6  // of which only six are chosen
+    int r = 6  // and of which six are special.
+    int y = 2  // probability to match all six
+    def result = CumulativeOperation.greaterThanOrEqual
+                                    .apply({ HyperGeometric.probability(N, n, r, it) }, y)
+
+    expect:
+    Math.abs(result - 0.145720452843487) < resolution
+  }
+
+
+  @Unroll
+  def "test cumulative #N samples, #n samplesChosen, #r specialSamples, P(y<#y) is #result"() {
+    setup:
+    System.out.println("test cumulative $N samples, $n samplesChosen, $r specialSamples, P(y<$y) " +
+        "is $result")
+
+    expect:
+    double probability = new HyperGeometric(lessThan, N, n, r).getResult(y)
+    Math.abs(probability - result) < resolution
+
+    where:
+    N    | n   | r   | y  | result
+    2    | 1   | 1   | 0  | 0d
+    2    | 1   | 1   | 1  | 0.5d
+    1    | 1   | 1   | 0  | 0d
+    1    | 1   | 1   | 1  | 0d
+    9    | 3   | 3   | 3  | 0.988095238095238d
+    55   | 31  | 5   | 3  | 0.377453351926161d
+    277  | 157 | 57  | 33 | 0.521499976361215d
+    301  | 300 | 30  | 30 | 0.0996677740863789d
+    1000 | 400 | 200 | 80 | 0.469133657102301d
+  }
+
   def "test cumulative high population"() {
     setup:
     def N = 80000
-    def n = 20000
-    def r = 40000
-    def y = 201
+    def n = 40000
+    def r = 10000
+    def y = 10000
     def result = new HyperGeometric(lessThanOrEqual, N, n, r).getResult(y)
-    println "hypergeometic result $result"
+    println "error is ${result - 1.0}"
     expect:
-    (1..n).forEach {
-      assert HyperGeometric.probability(N, n, r, it) < resolution: "y = $it";
-    }
+    assert result != 0
+    Math.abs(result -  1.0)< resolution
+  }
+
+  def "test chance to win CA Lotto jackpot"() {
+    setup:
+    int N = 47  // five numbers between 1 to 47
+    int n = 5
+    int r = 5
+    int y = 5
+    def result = HyperGeometric.probability(N, n, r, y)
+    // mega number is 1-27, of which we need one. so 1/27.
+    result = result / 27
+    println "chance to win jackpot is 1 in ${(int)DiscreteProbability.reciprocal(result)}"
+    expect:
+    // ca lotto web site says roughly 1 in 42million
+    Math.abs(result - 1/42000000) <  1.0E-9   // within a million
   }
 }
