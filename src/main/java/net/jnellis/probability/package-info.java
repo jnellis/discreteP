b89b0bd980fdf693faea28ab88fe3facc42a7c43
Joe Nellis <joe@jnellis.net>
2015-09-06 14:13:47 -0700

    package level javadoc

---
+++ src/main/java/net/jnellis/probability/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * package-info.java
+ *
+ * Copyright (c) 2015. Joe Nellis
+ * Distributed under MIT License. See accompanying file License.txt or at
+ * http://opensource.org/licenses/MIT
+ */
+
+/**
+ * A set of probability distribution functions. Probabilities can be held as
+ * objects or can be computed using static methods. Cumulative probabilities
+ * are computed as the sum of discrete probability results.
+ */
+package net.jnellis.probability;
