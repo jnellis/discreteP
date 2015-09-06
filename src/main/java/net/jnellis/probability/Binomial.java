3ff87ee826e2e71ecf0b8baf8f568932d97b3b30
Joe Nellis <joe@jnellis.net>
2015-09-05 20:50:57 -0700

    Add more comprehensive tests. Tweak implementation of #probability.  Javadocs added.

--- src/main/java/net/jnellis/probability/Binomial.java
+++ src/main/java/net/jnellis/probability/Binomial.java
@@ -1,15 +1,63 @@
+/*
+ * Binomial.java
+ *
+ * Copyright (c) 2015. Joe Nellis
+ * Distributed under MIT License. See accompanying file License.txt or at
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.jnellis.probability;
 
 /**
- * User: Joe Nellis
- * Date: 8/9/2015
- * Time: 8:37 PM
+ * The <a href="https://en.wikipedia.org/wiki/Binomial_distribution">
+ * Binomial probability distribution.</a>
+ * <p>
+ * Use when you need to ask the question,
+ * "If the chance of something happening is P percent,
+ * what is the probability it will happen Y times out of
+ * N attempts."
+ * Example: Die rolling. If the chance to roll a one is
+ * 1/6 or 16.7%, what is the probability it will happen 3 times
+ * out of 5 rolls.
+ * <p>
+ * Notes: The event must be a discrete event like rolling a die,
+ * flipping a coin, or pulling a card.  Events that consider an
+ * average percentage that it will happen or if each trial in turn
+ * affects the percentage that the event will happen are not
+ * modeled by the binomial probability distribution but can give
+ * meaningful estimates. ex: A veteran NBA basketball player has a free throw
+ * percentage of 70%. What is the probability he makes his next two
+ * free throw shots. His first shot would then change his free throw
+ * percentage by a very small amount but if this fact is omitted the answer is
+ * relatively the same.
+ * </p>
+ * <pre>
+ * // roll 5 dice, what is probability exactly three of them are the same value.
+ * double probability = Binomial.probability(5, 0.166666, 3);
+ *
+ * // roll 5 dice, what is probability 3 or more dice are all the same value.
+ * // aka 3-of-a-kind, 4-of-a-kind or 5-of-a-kind.
+ * Binomial pdf = new Binomial(greaterThanOrEqual, 5, 0.166666);
+ * double result = pdf.getResult(3);
+ *
+ * // or from a static context that creates no Binomial object
+ * result = CumulativeOperation.greaterThanOrEqual
+ *                             .apply(3, y -&gt; Binomial.probability(5, 0
+ *                             .166666, y));
+ * </pre>
  */
 public class Binomial extends DiscreteProbability {
 
   private final int trials;
   private final double chanceOfSuccess;
 
+  /**
+   * Creates a representation of a binomial probability distribution.
+   *
+   * @param rvOperation     The cumulative operation on this probability
+   * @param trials          Number of times the experiment takes place
+   * @param chanceOfSuccess The probability of the successful event.
+   */
   public Binomial(CumulativeOperation rvOperation,
                   int trials,
                   double chanceOfSuccess) {
@@ -22,101 +70,104 @@ public class Binomial extends DiscreteProbability {
     this.chanceOfSuccess = chanceOfSuccess;
   }
 
-  public final boolean nonNegative(int number) {
-    return number >= 0;
-  }
+  /**
+   */
 
-  public final boolean betweenZeroAndOneInclusive(double probability) {
-    return probability >= 0 && probability <= 1.0;
+  @Override
+  public double computeResult(int randomVariable) {
+    return probability(trials, chanceOfSuccess, randomVariable);
   }
 
   /**
-   * The discrete binomial probability.
+   * The binomial probability at P(randomVariable)
+   * <p>
    * The equation for this probability is:
+   * <pre>
+   * <code>
    * P(Y) = n! /((n-y)! * y!) * p^(y) * q^(n-y)
+   * </code>
    * where n is the number of trials,
-   * y is the number of successful trials, aka randomVariable,
-   * p is the chance of a trial succeeding,
-   * q is the chance of a trial failing, (1 - p).
+   *       p is the chance of a trial succeeding,
+   *       q is the chance of a trial failing, (1 - p),
+   *       y is the number of successful trials, aka randomVariable,
+   * </pre>
+   *
+   * @param trials          Number of trials of this experiment
+   * @param chanceOfSuccess chance each trial succeeds
+   * @param randomVariable  number of successes to investigate
+   * @return The probability of P(Y = randomVariable)
    */
-  @Override
-  public double computeResult(int randomVariable) {
+  public static double probability(final int trials,
+                                   final double chanceOfSuccess,
+                                   final int randomVariable) {
     // no trials, negative successes, or more successes than possible,
     // has zero probability anything happened.
     if (trials == 0 || randomVariable < 0 || randomVariable > trials) {
       return 0.0;
     }
 
-    // initialize some variables
-    int n = trials;
-    int y = randomVariable;
-    double p = chanceOfSuccess;
-    double q = 1.0 - chanceOfSuccess;
+    final double chanceOfFailure = 1.0 - chanceOfSuccess;
 
     // Early Optimizations:
     // The factorial and P components cancel out leaving the Q component.
-    if (y == 0) {
-      return Math.pow(q, n);
+    if (randomVariable == 0) {
+      return Math.pow(chanceOfFailure, trials);
     }
     // The factorial and Q components cancel out leaving the P component.
-    if (y == n) {
-      return Math.pow(p, y);
+    if (randomVariable == trials) {
+      return Math.pow(chanceOfSuccess, randomVariable);
     }
 
     // In the factorial part of the equation [ n!/((n-y)!y!) ],
     // we can easily cancel out (n-y)! or y! but not both so
     // choose the larger of the two denominators to cancel out and
     // keep the smaller denominator component.
-    int range = Integer.min(n - y, y);
+    final int range = Integer.min(trials - randomVariable, randomVariable);
+    final int numerFloor = trials - range;
 
-    // counters of p, q, numerators, and denominators
-    int pees = y;
-    int ques = n - y;
+    // counters of p, q, numerator and denominator factors
+    int successes = randomVariable;
+    int failures = trials - randomVariable;
     int denom = range;
-    int numer = n;
-    int numerFloor = n - range;
-    double result = 1.0;
+    int numer = trials;
 
     // While loop soup - computing the result by multiplying by components
     // of the equation in order to maintain an intermediate result that
     // floats around 1.0 if possible.
-    while (pees > 0 || numer > numerFloor) {
+    double result = 1.0;
+    while (successes > 0 || numer > numerFloor) {
       if (result >= 1.0 || numer == numerFloor) { // lowering values
         if (denom > 1) {
           result = result / denom;
           denom--;
-        } else if (ques > 0) {
-          result *= q;
-          ques--;
-        } else if (pees > 0) {
-          result *= p;
-          pees--;
+        } else if (failures > 0) {
+          result *= chanceOfFailure;
+          failures--;
         } else {
-          throw new IllegalStateException("unexpected end of small values");
+          result *= chanceOfSuccess;
+          successes--;
         }
-
       } else { //increasing values
-        if (numer > numerFloor) {
-          result *= numer;
-          numer--;
-        } else {
-          throw new IllegalStateException("unexpected end of large values");
-        }
+        result *= numer;
+        numer--;
       }
     }
     return result;
   }
 
-
-  // The Expected Value or population mean is defined
-  // by E(V) = N*p
+  /**
+   * The Expected Value or population mean is defined  by
+   * <code>E(V) = trials * chanceOfSuccess</code>
+   */
   @Override
   public double getExpectedValue() {
     return trials * chanceOfSuccess;
   }
 
-  // the Variance is defined by
-  // o^2 = N*p*q
+  /**
+   * The Variance is defined by
+   * <code>o^2 = trials * chanceOfSuccess * (1-chanceOfSuccess)</code>
+   */
   @Override
   public double getVariance() {
     return trials * chanceOfSuccess * (1.0 - chanceOfSuccess);
--- src/test/groovy/net/jnellis/probability/BinomialTest.groovy
+++ src/test/groovy/net/jnellis/probability/BinomialTest.groovy
@@ -1,6 +1,15 @@
+/*
+ * BinomialTest.groovy
+ *
+ * Copyright (c) 2015. Joe Nellis
+ * Distributed under MIT License. See accompanying file License.txt or at
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.jnellis.probability
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static net.jnellis.probability.CumulativeOperation.*
 
@@ -12,7 +21,6 @@ import static net.jnellis.probability.CumulativeOperation.*
  */
 class BinomialTest extends Specification {
 
-  def binomial
   double resolution = 1.0E-12
 
   def "test simple equals probability"() {
@@ -38,7 +46,51 @@ class BinomialTest extends Specification {
     8  | 0.00390625
   }
 
-  def "test cumulative probabilty"() {
+  @Unroll
+  def "test #N trials of #p success each with P(Y=#rv) = #result"() {
+
+    expect:
+    Math.abs(Binomial.probability(N, p, rv) - result) < resolution
+
+    where:
+    N   | p           | rv  | result
+    1   | 0.0         | 0   | 1d
+    1   | 0.0         | 1   | 0
+    1   | 1.0d        | 0   | 0
+    1   | 1.0d        | 1   | 1d
+    2   | 1.0d        | 0   | 0
+    2   | 1.0d        | 1   | 0
+    2   | 1.0d        | 2   | 1d
+    17  | 0.5d        | 7   | 0.14837646484375d
+    67  | 0.99d       | 67  | 0.509985746249565d
+    67  | 0.01d       | 67  | 0
+    600 | 1.0d / 3.0d | 200 | 0.0345326241871131d
+  }
+
+  @Unroll
+  def "test #N trials of #p success each with P(Y<#rv) = #result"() {
+    expect:
+    double probability = lessThan.apply(rv) { Binomial.probability(N, p, it) }
+
+    Math.abs(probability - result) < resolution
+
+    where:
+    N   | p           | rv  | result
+    1   | 0.0         | 0   | 0
+    1   | 0.0         | 1   | 1
+    1   | 1.0         | 0   | 0
+    1   | 1.0         | 1   | 0
+    2   | 1.0         | 0   | 0
+    2   | 1.0         | 1   | 0
+    2   | 1.0         | 2   | 0
+    17  | 0.5         | 7   | 0.166152954101563
+    67  | 0.99        | 67  | 0.490014253750435
+    67  | 0.01        | 67  | 1
+    600 | 1.0d / 3.0d | 200 | 0.484649326421801
+  }
+
+
+  def "test cumulative operation to itself"() {
     setup:
     def trials = 8
     def chanceOfSuccess = 0.5
@@ -70,15 +122,11 @@ class BinomialTest extends Specification {
     setup:
     def trials = 8009
     def chanceOfSuccess = 0.5
-    def result =
-        new Binomial(lessThanOrEqual, trials, chanceOfSuccess).getResult(rv)
-    println result
-    println 0.473273430936
+    def result = new Binomial(lessThanOrEqual, trials, chanceOfSuccess).getResult(rv)
 
     expect:
-    Math.abs(
-        result - 0.473273430936
-    ) < resolution * 1E5 // 4001 adds of small decimal numbers loses resolution
+    Math.abs(result - 0.473273430936) < resolution * 1E5
+    // 4001 adds of small decimal numbers loses resolution
     where:
     rv << 4001
 
@@ -88,28 +136,24 @@ class BinomialTest extends Specification {
     setup:
     def trials = 80009
     def chanceOfSuccess = 0.5
-    def result = new Binomial(equal, trials, chanceOfSuccess).getResult(rv)
-    println result
-    println 0.0028199161817
+    def result = Binomial.probability(trials, chanceOfSuccess, rv);
+
     expect:
-    Math.abs(
-        result - 0.0028199161817
-    ) < resolution * 1E3 // Here the resolution loss is from 5 digits in N.
+    Math.abs(result - 0.0028199161817) < resolution * 1E3
+    // Here the resolution loss is from 5 digits in N.
     where:
     rv << 40001
 
   }
 
-  def "test cumulative probabilty with medium number of trials"() {
+  def "test cumulative probabilty with medium high number of trials"() {
     setup:
     def trials = 997
     def chanceOfSuccess = 0.55
 
     expect:
-    Math.abs(
-        new Binomial(lessThanOrEqual, trials, chanceOfSuccess).getResult(rv)
-            - 0.00219083926527885
-    ) < resolution
+    double result = new Binomial(lessThanOrEqual, trials, chanceOfSuccess).getResult(rv)
+    Math.abs(result - 0.00219083926527885) < resolution
     where:
     rv << 503
 
