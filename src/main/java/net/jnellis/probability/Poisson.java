4d1cbfba4a6ee2652cbb797b3442f912bdeb595a
Joe Nellis <joe@jnellis.net>
2015-09-06 17:22:52 -0700

    Add more tests, streamline probability implementation, added javadocs.

--- src/main/java/net/jnellis/probability/Poisson.java
+++ src/main/java/net/jnellis/probability/Poisson.java
@@ -9,14 +9,43 @@
 package net.jnellis.probability;
 
 /**
- * User: Joe Nellis
- * Date: 8/21/2015
- * Time: 10:33 AM
+ * The <a href="https://en.wikipedia.org/wiki/Poisson_distribution">
+ * Poisson probability distribution.</a>
+ * <p>
+ * Use when you have events that can happen instantaneously
+ * and at any time. The chance of two or more events happening
+ * at the same time is effectively zero for a time span small
+ * enough.
+ * <p>
+ * For example: Over the years, a certain street corner has noted an
+ * average of 7 traffic accidents per month.  What is the probability that
+ * there will be double the amount of accidents the following month.  Here our
+ * lambda is 7, the average from which we base the likelihood
+ * of traffic accidents happening in a month. The random variable is 14, double
+ * the amount of accidents expected.
+ * <p>
+ * <pre>
+ * double result = Poisson.probability(7,14);  // less than 1% chance.
+ * </pre>
+ * <p>
+ * What is the chance there will be 3 or less accidents next month?
+ * <pre>
+ * double result = new Poisson(lessThanOrEqual, 7).getResult(3) // 8.2% chance
+ * // or by not creating a Poisson object, using static methods
+ * result = CumulativeOperation.lessThanOrEqual
+ *                             .apply(3, y-&gt; Poisson.probability(7,y));
+ * </pre>
  */
 public class Poisson extends DiscreteProbability {
 
   private final double lambda;
 
+  /**
+   * Creates a representation of a Poisson probability distribution.
+   *
+   * @param rvOperation The cumulative operation to be applied
+   * @param lambda      The average rate of success
+   */
   public Poisson(CumulativeOperation rvOperation,
                  double lambda) {
     super(rvOperation);
@@ -33,14 +62,22 @@ public class Poisson extends DiscreteProbability {
     return lambda;
   }
 
-  /*
-  The Poisson probability.
-    The equation for this probability is:
-    P(Y) = lambda^Y * e^-lambda / Y!
-  */
   @Override
   public double computeResult(int randomVariable) {
-    // split eulers exponent into integral and fractional parts
+    return probability(lambda, randomVariable);
+  }
+
+  /**
+   * The Poisson probability.
+   * The equation for this probability is:
+   * P(Y) = (lambda^Y) * (e^-lambda) / Y!
+   *
+   * @param lambda         Average rate of success
+   * @param randomVariable how many successes of interest
+   * @return The probability of this event.
+   */
+  public static double probability(double lambda, int randomVariable) {
+    // split Eulers exponent into integral and fractional parts
     int integral = (int) lambda;
     double fractional = lambda - integral;
     //counters
@@ -50,25 +87,19 @@ public class Poisson extends DiscreteProbability {
     // start with the fractional component, it's between 0 and 1.0
     double result = Math.exp(-1.0 * fractional);
 
-    while (integral > 0 || lambdas > 0 || denoms > 0) {
+    while (integral > 0 || lambdas > 0) {
       //lowering values
       if (result >= 1.0 || lambdas == 0) {
         if (denoms > 0) {
           result /= denoms;
           denoms--;
-        } else if (integral > 0) {
+        } else {
           result *= Math.exp(-1.0);
           integral--;
-        } else {
-          throw new IllegalStateException("unexpected end of small values");
         }
       } else { // increasing values
-        if (lambdas > 0) {
-          result *= lambda;
-          lambdas--;
-        } else {
-          throw new IllegalStateException("premature end of power component");
-        }
+        result *= lambda;
+        lambdas--;
       }
     }
     return result;
--- src/test/groovy/net/jnellis/probability/PoissonTest.groovy
+++ src/test/groovy/net/jnellis/probability/PoissonTest.groovy
@@ -1,6 +1,16 @@
+/*
+ * PoissonTest.groovy
+ *
+ * Copyright (c) 2015. Joe Nellis
+ * Distributed under MIT License. See accompanying file License.txt or at
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.jnellis.probability
 
 import spock.lang.Specification
+import spock.lang.Unroll
+
 import static net.jnellis.probability.CumulativeOperation.*
 
 /**
@@ -10,15 +20,48 @@ import static net.jnellis.probability.CumulativeOperation.*
  *
  */
 class PoissonTest extends Specification {
-  double  resolution = 1.0E-12
+  double resolution = 1.0E-12
+
+  @Unroll
+  def "test Poisson pdf with lambda: #lambda, P(Y=#y) = #result"() {
+
+    expect:
+    def probability = Poisson.probability(lambda, y)
+    Math.abs(probability - result) < resolution
+
+    where:
+    lambda      | y | result
+    0.1         | 0 | 0.90483741803596
+    0.1         | 1 | 0.090483741803596
+    1           | 0 | 0.367879441171442
+    1           | 1 | 0.367879441171442
+    Math.exp(1) | 0 | 0.0659880358452496
+    Math.exp(1) | 1 | 0.179374078733909
+  }
+
+  @Unroll
+  def "test Poisson cdf with lambda: #lambda, P(Y&lt;=#y) = #result"() {
+
+    expect:
+    def probability = lessThan.apply(y) { Poisson.probability(lambda, it) }
+    Math.abs(probability - result) < resolution
+
+    where:
+    lambda      | y | result
+    0.1         | 0 | 0.0
+    0.1         | 1 | 0.90483741803596
+    1           | 0 | 0.0
+    1           | 1 | 0.367879441171442
+    Math.exp(1) | 0 | 0.0
+    Math.exp(1) | 1 | 0.06598803584524
+  }
 
   def "test cumulative probability against TI-89 function TIStat.poissCdf"() {
     setup:
-    double poissCdf =  0.423125414419
+    double poissCdf = 0.423125414419
     double lambda = 5.1
     int Y = 4
     double result = new Poisson(lessThanOrEqual, lambda).getResult(Y)
-    println result
 
     expect:
     Math.abs(result - poissCdf) < resolution
@@ -26,11 +69,10 @@ class PoissonTest extends Specification {
 
   def "test exact probability against TI-89 function TIStat.poissPdf"() {
     setup:
-    double poissPdf =  0.051648853532
+    double poissPdf = 0.051648853532
     double lambda = 20
     int Y = 15
     double result = new Poisson(equal, lambda).getResult(Y)
-    println result
 
     expect:
     Math.abs(result - poissPdf) < resolution
@@ -38,13 +80,14 @@ class PoissonTest extends Specification {
 
   def "test extreme exact probability against TI-89 function TIStat.poissPdf"() {
     setup:
-    double poissPdf =  0.00140910681
+    double poissPdf = 0.00140910681
     double lambda = 79999
     int Y = 80011
     double result = new Poisson(equal, lambda).getResult(Y)
-    println result
 
     expect:
     Math.abs(result - poissPdf) < resolution
   }
+
+
 }
